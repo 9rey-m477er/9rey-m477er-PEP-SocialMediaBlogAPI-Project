4d000351e81307fa40b23a851ad92948b3c1ac4b
9rey-m477er <9rey-m477er@github.com>
2026-07-01 00:28:45 +0000

    Added DAO classes

--- src/main/java/Service/accountService.java
+++ src/main/java/Service/accountService.java
@@ -1,5 +1,14 @@
 package Service;
 
+import DAO.accountDao;
+
 public class accountService {
+  public accountDAO accountDao;
   
+  public accountService(){
+    
+  }
+  public void addAccount(accountDAO.account){
+
+  }
 }
