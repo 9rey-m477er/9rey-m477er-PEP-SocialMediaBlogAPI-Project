f0fd8c36a768c9ae8b15ad2aa339b851f0bd3b7b
9rey-m477er <9rey-m477er@github.com>
2026-07-07 02:30:16 +0000

    Test run: 2/23 passed, 21 failed, 0 skipped - 2026-07-07T02:30:16.435Z

--- src/main/java/DAO/messageDAO.java
+++ src/main/java/DAO/messageDAO.java
@@ -11,11 +11,12 @@ public class messageDAO {
   public Message createMessage(Message m){
     Connection connection = ConnectionUtil.getConnection();
     try{
-      String query = "INSERT INTO message (message_text, posted_by, time_posted_epoch) VALUES (?,?,?)";
+      String query = "INSERT INTO message (message_text, posted_by, time_posted_epoch, message_id) VALUES (?,?,?,?)";
       PreparedStatement ps = connection.prepareStatement(query);
       ps.setString(1, m.getMessage_text());
       ps.setInt(2, m.getPosted_by());
       ps.setLong(3, m.getTime_posted_epoch());
+      ps.setInt(4, m.getMessage_id());
       ps.executeUpdate();
       return m;
     }
