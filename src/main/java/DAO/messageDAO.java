9030663a236f6355b84d85adf33b3c3c6d1d4bbe
9rey-m477er <9rey-m477er@github.com>
2026-07-07 02:42:00 +0000

    Test run: 3/23 passed, 20 failed, 0 skipped - 2026-07-07T02:41:59.987Z

--- src/main/java/DAO/messageDAO.java
+++ src/main/java/DAO/messageDAO.java
@@ -11,20 +11,26 @@ public class messageDAO {
   public Message createMessage(Message m){
     Connection connection = ConnectionUtil.getConnection();
     try{
-      String query = "INSERT INTO message (message_text, posted_by, time_posted_epoch, message_id) VALUES (?,?,?,?)";
-      PreparedStatement ps = connection.prepareStatement(query);
+      String query = "INSERT INTO message (message_text, posted_by, time_posted_epoch) VALUES (?,?,?)";
+      PreparedStatement ps = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
       ps.setString(1, m.getMessage_text());
       ps.setInt(2, m.getPosted_by());
       ps.setLong(3, m.getTime_posted_epoch());
-      ps.setInt(4, m.getMessage_id());
       ps.executeUpdate();
-      return m;
+      
+      ResultSet rs = ps.getGeneratedKeys();
+      int message_id = 0;
+      if(rs.next()){
+        message_id = (int) rs.getInt(1);
+      }
+      return new Message(message_id, m.getPosted_by(), m.getMessage_text(), m.getTime_posted_epoch());
     }
     catch(SQLException e){
       System.out.println(e.getMessage());
     }
     return null;
   }
+
   public Message deleteMessage(Message m){
     return null;
   }
