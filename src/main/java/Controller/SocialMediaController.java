60544d16c1451f911c44e1d99c02c41e71266770
9rey-m477er <9rey-m477er@github.com>
2026-07-08 02:01:08 +0000

    Test run: 0/23 passed, 23 failed, 0 skipped - 2026-07-08T02:01:08.147Z

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -76,6 +76,7 @@ public class SocialMediaController {
         }
         else{
             ctx.status(400);
+            ctx.result("");
         }
     }
     private void getMessageHandler(Context ctx) throws JsonProcessingException{
